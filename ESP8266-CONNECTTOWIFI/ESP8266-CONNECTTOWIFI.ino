bd426dd7fd944e286b601c7a2d30e609a8ea806d
cyfinfaza <cyfinfaza@gmail.com>
2020-02-22 07:07:50 -0500

    ESP8266
    Some of the files we used for the meshing system.

---
+++ ESP8266-CONNECTTOWIFI/ESP8266-CONNECTTOWIFI.ino
@@ -0,0 +1,51 @@
+#include "ESP8266WiFi.h"
+#include <painlessMesh.h>
+// WiFi parameters to be configured
+const char* ssid = "Cy's Laptop";
+const char* password = "lenovoyoga720";
+#define   MESH_SSID       "whateverYouLike"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
+
+void sendMessage(); 
+void receivedCallback(uint32_t from, String & msg);
+void newConnectionCallback(uint32_t nodeId);
+void changedConnectionCallback(); 
+void nodeTimeAdjustedCallback(int32_t offset); 
+void delayReceivedCallback(uint32_t from, int32_t delay);
+
+Scheduler     userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+bool calc_delay = false;
+SimpleList<uint32_t> nodes;
+
+void sendMessage() ; // Prototype
+Task taskSendMessage( TASK_SECOND * 1, TASK_FOREVER, &sendMessage ); // start with a one second interval
+
+// Task to blink the number of nodes
+Task blinkNoNodes;
+bool onFlag = false;
+
+
+void setup(void)
+{ 
+  Serial.begin(9600);
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  // while wifi not connected yet, print '.'
+  // then after it connected, get out of the loop
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+  }
+  //print a new line, then print WiFi connected and the IP address
+  Serial.println("");
+  Serial.println("WiFi connected");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+}
+void loop() {
+  // Nothing
+}
